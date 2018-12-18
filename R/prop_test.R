b21fa4f92c9b0f790b645dfa4a557d30eb040e80
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-12-18 16:04:34 -0500

    writing "methods" for prop_test instead of a bunch of if-else
    using method loosely, technically not writing methods in terms of oop

--- R/prop_test.R
+++ R/prop_test.R
@@ -5,52 +5,10 @@ prop_test <- function(x, n, p = NULL, method = c("wald", "wilson"),
 
   method <- match.arg(method)
   alternative <- match.arg(alternative)
-  parameter <- 1
-  names(parameter) <- "df"
 
   if (method == "wald") {
-    if (correct) {
-      stop("Nick hasn't gotten this far yet")
-    } else {
-      method <- "1 sample test of proportions without continuity correction"
-
-      p_mle <- x / n
-      names(p_mle) <- "MLE proportion"
-
-      se_ci <- sqrt(p_mle * (1 - p_mle) / n)
-      z_cv <- qnorm(0.5 * (1 + conf.level))
-      ci <- round(p_mle + c(-1, 1) * z_cv * se_ci, 4)
-      attr(ci, "conf.level") <- conf.level
-
-      if (!is.null(p)) {
-        se <- sqrt(p * (1 - p) / n)
-        statistic <- (p_mle - p) / se
-      } else {
-        p <- 0.5
-        se <- sqrt(p * (1 - p) / n)
-        statistic <- (p_mle - p) / se
-
-      }
-      names(statistic) <- "Z"
-      names(p) <- "proportion"
-
-      if (alternative == "two.sided") {
-        if (statistic < 0) {
-          p.value <- pnorm(statistic) * 2
-        } else {
-          p.value <- pnorm(statistic, lower.tail = FALSE) * 2
-        }
-      } else if (alternative == "less") {
-        p.value <- pnorm(statistic)
-      } else if (alternative == "greater") {
-        p.value <- pnorm(statistic, lower.tail = FALSE)
-      }
-    }
-    vals <- list(null.value = p, alternative = alternative, method = method,
-                 estimate = p_mle, data.name = "Test", statistic = statistic,
-                 p.value = p.value, conf.int = ci)
-    class(vals) <- "htest"
-    print(vals)
+    prop_test.wald(x = x, n = n, p = p, alternative = alternative,
+                   conf.level = conf.level, correct = correct)
 
   } else if (method == "wilson") {
     prop.test(x = x, n = n, p = p, alternative = alternative,
