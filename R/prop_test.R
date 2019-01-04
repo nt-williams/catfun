537fbfefeab1a604dc9b9317f8f09fc4c620cc3d
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-01-04 14:10:24 -0500

    updated description, documentation, and direct call to DescTools

--- R/prop_test.R
+++ R/prop_test.R
@@ -41,62 +41,62 @@ prop_test <- function(x, n, p = NULL, method = c("wald", "wilson", "agresti-coul
 
   if (method == "wald") {
 
-    ci <- BinomCI(x, n, method = "wald", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "wald", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "wilson") {
 
-    ci <- BinomCI(x, n, method = "wilson", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "wilson", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "agresti-couli") {
 
-    ci <- BinomCI(x, n, method = "agresti-couli", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "agresti-couli", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "jeffreys") {
 
-    ci <- BinomCI(x, n, method = "jeffreys", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "jeffreys", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "modified wilson") {
 
-    ci <- BinomCI(x, n, method = "modified wilson", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "modified wilson", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "wilsoncc") {
 
-    ci <- BinomCI(x, n, method = "wilsoncc", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "wilsoncc", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "modified jeffreys") {
 
-    ci <- BinomCI(x, n, method = "modified jeffreys", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "modified jeffreys", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "clopper-pearson") {
 
-    ci <- BinomCI(x, n, method = "clopper-pearson", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "clopper-pearson", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "arcsine") {
 
-    ci <- BinomCI(x, n, method = "arcsine", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "arcsine", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "logit") {
 
-    ci <- BinomCI(x, n, method = "logit", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "logit", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "witting") {
 
-    ci <- BinomCI(x, n, method = "witting", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "witting", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   } else if (method == "pratt") {
 
-    ci <- BinomCI(x, n, method = "pratt", conf.level = conf.level)
+    ci <- DescTools::BinomCI(x, n, method = "pratt", conf.level = conf.level)
     method_ci <- ci[1, 2:3]
 
   }
