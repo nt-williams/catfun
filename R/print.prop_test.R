1d31a27a05702b7fdf370f0ecee1b2e40f189018
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-01-09 16:49:58 -0500

    prop_test performing k-sample tests of proportions

--- R/print.prop_test.R
+++ R/print.prop_test.R
@@ -1,23 +1,39 @@
 
+#' @export
+
 print.prop_test <- function(z, ...) {
-  cat(z$x, " out of ", paste0(z$n, ","), " null probability = ", z$p)
-  cat("\n")
-  cat(paste(rep("-", 40), collapse = ""), "\n")
-  cat("Observed proportion:", z$estimate, "\n")
-  cat("Confidence interval method:", z$method, "\n")
-  cat("Confidence interval:", z$method_ci, "\n")
-  cat("\n")
-  cat("Exact limits:", z$exact_ci, "\n")
-  cat("\n")
-  cat("Test that", z$estimate, "=", z$p, "\n")
-  cat(paste(rep("-", 40), collapse = ""), "\n")
-  cat("Chi-squared:", z$statistic, "\n")
-  cat("Degrees freedom:", z$df, "\n")
-  cat("p-value:", z$p_value, "\n")
-  cat("\n")
-  if (!is.null(z$exact_p)) {
-    cat("Exact p-value:", z$exact_p, "\n")
+  if (length(z$x) == 1) {
+    cat(z$x, " out of ", paste0(z$n, ","), " null probability = ", z$p)
+    cat("\n")
+    cat(paste(rep("-", 40), collapse = ""), "\n")
+    cat("Observed proportion:", z$estimate, "\n")
+    cat("Confidence interval method:", z$method, "\n")
+    cat("Confidence interval:", z$method_ci, "\n")
+    cat("\n")
+    cat("Exact limits:", z$exact_ci, "\n")
+    cat("\n")
+    cat("Test that", z$estimate, "=", z$p, "\n")
+    cat(paste(rep("-", 40), collapse = ""), "\n")
+    cat("Chi-squared:", z$statistic, "\n")
+    cat("Degrees freedom:", z$df, "\n")
+    cat("p-value:", z$p_value, "\n")
+    cat("\n")
+    if (!is.null(z$exact_p)) {
+      cat("Exact p-value:", z$exact_p, "\n")
+    }
+    cat("\n")
+  } else {
+    cat("Observed proportions:", "\n")
+    cat_bullet(z$estimate)
+    cat_line()
+    cat("Confidence interval method:", z$method, "\n")
+    cat("Confidence intervals:", "\n")
+    print(z$method_ci)
+    cat_rule()
+    cat(length(z$x), "sample test for equality of proportions", "\n")
+    cat("Chi-squared:", z$statistic, "\n")
+    cat("p-value:", z$p_value)
   }
-  cat("\n")
+
 }
 
--- R/prop_test.R
+++ R/prop_test.R
@@ -2,6 +2,8 @@
 #' Tests for equality of proportions
 #'
 #' Run tests for equality of proportions
+#'
+#' @export
 
 
 prop_test <- function(x, n, p = NULL, method = c("wald", "wilson", "agresti-couli", "jeffreys",
@@ -13,19 +15,24 @@ prop_test <- function(x, n, p = NULL, method = c("wald", "wilson", "agresti-coul
   method <- match.arg(method)
   alternative <- match.arg(alternative)
 
-  if (!is.null(p)) {
-    p <- p
-  } else {
-    p <- 0.5
-  }
+  if (length(x) == 1) {
+    if (!is.null(p)) {
+      p <- p
+    } else {
+      p <- 0.5
+    }
 
-  exact_test <- binom.test(x, n, alternative = alternative, conf.level = conf.level)
-  if (exact == TRUE) {
-    exact_p <- exact_test$p.value
-  } else {
+    exact_test <- binom.test(x, n, alternative = alternative, conf.level = conf.level)
+    if (exact == TRUE) {
+      exact_p <- exact_test$p.value
+    } else {
+      exact_p <- NULL
+    }
+    exact_ci <- exact_test$conf.int[1:2]
+  } else if (length(x) > 1 && is.null(p)) {
+    exact_ci <- NULL
     exact_p <- NULL
   }
-  exact_ci <- exact_test$conf.int[1:2]
 
   if (correct == FALSE) {
     test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = FALSE)
@@ -37,74 +44,80 @@ prop_test <- function(x, n, p = NULL, method = c("wald", "wilson", "agresti-coul
   statistic <- test$statistic
   df <- test$parameter
   p_value <- test$p.value
-  estimate <- test$estimate
+  estimate <- round(test$estimate, 4)
 
   if (method == "wald") {
 
     ci <- DescTools::BinomCI(x, n, method = "wald", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "wilson") {
 
     ci <- DescTools::BinomCI(x, n, method = "wilson", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "agresti-couli") {
 
     ci <- DescTools::BinomCI(x, n, method = "agresti-couli", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "jeffreys") {
 
     ci <- DescTools::BinomCI(x, n, method = "jeffreys", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "modified wilson") {
 
     ci <- DescTools::BinomCI(x, n, method = "modified wilson", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "wilsoncc") {
 
     ci <- DescTools::BinomCI(x, n, method = "wilsoncc", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "modified jeffreys") {
 
     ci <- DescTools::BinomCI(x, n, method = "modified jeffreys", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "clopper-pearson") {
 
     ci <- DescTools::BinomCI(x, n, method = "clopper-pearson", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "arcsine") {
 
     ci <- DescTools::BinomCI(x, n, method = "arcsine", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "logit") {
 
     ci <- DescTools::BinomCI(x, n, method = "logit", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "witting") {
 
     ci <- DescTools::BinomCI(x, n, method = "witting", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
   } else if (method == "pratt") {
 
     ci <- DescTools::BinomCI(x, n, method = "pratt", conf.level = conf.level)
-    method_ci <- ci[1, 2:3]
+    method_ci <- ci[, 2:3]
 
+  }
+
+  if (length(x) > 1) {
+    rownames(method_ci) <- c()
+    method_ci <- as.data.frame(method_ci)
+    names(method_ci) <- c("Lower bound", "Upper bound")
   }
 
   out <- list(x = x, n = n, p = p, estimate = estimate, method = method, method_ci = method_ci,
               exact_ci = exact_ci, exact_p = exact_p, statistic = statistic, df = df, p_value = p_value)
   class(out) <- "prop_test"
-  print(out)
+  out
 }
 
 
