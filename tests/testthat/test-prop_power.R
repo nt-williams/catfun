d43515397fc72734e51424166d75875682e59951
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-01-28 11:44:57 -0500

    prop_power error tests

---
+++ tests/testthat/test-prop_power.R
@@ -0,0 +1,14 @@
+
+context("prop_power is working")
+library(catfun)
+
+test_that("errors are occuring correctly", {
+  expect_error(prop_power(n = 220, n1 = 110, p1 = 0.35, p2 = 0.2),
+               "If using a balanced design, n1 and n2 must be left blank")
+  expect_error(prop_power(p1 = 0.35, p2 = 0.2, n = 220, odds.ratio = 0.4642857),
+               "Either manually enter p2 or use odds ratio, not both")
+  expect_error(prop_power(p1 = 0.35, p2 = 0.2, n = 220, percent.reduction = 42.857),
+               "Either manually enter p2 or use percent reduction, not both")
+  expect_error(prop_power(n = 220, p1 = 0.35, p2 = 0.2, fraction = 2/3, power = 0.85),
+               "If given power, n, n1, and n2 must be left blank")
+})
