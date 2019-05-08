32cfe34d40bb6521f0b74a784fc4ef3a9ba2eda5
Nick Williams <ntwilliams.personal@gmail.com>
2019-05-08 15:28:04 -0400

    refactored riskdiff to use S3

--- R/riskdiff.R
+++ R/riskdiff.R
@@ -2,13 +2,12 @@
 #'
 #' Calculate risk difference and 95 percent confidence interval using Wald method.
 #'
-#' @param df a one-dimensional dataframe containing counts, a two-dimensional dataframe with binary variables x and y, or matrix of counts.
-#'      If a matrix, x and y must be NULL
-#' @param x predictor/exposure, vector.
-#' @param y outcome, vector.
-#' @param weight an optional vector of count weights.
+#' @param df a dataframe with binary variables x and y or a 2 x 2 frequency table/matrix. If a table or matrix, x and y must be NULL.
+#' @param x predictor/exposure, vector. Must be blank if df is a table or matrix.
+#' @param y outcome, vector. Must be blank if df is a table or matrix.
+#' @param weight an optional vector of count weights. Must be blank if df is a table or matrix.
 #' @param conf.level confidence level for confidence interval, default is 0.95.
-#' @param rev reverse order of cells. Options are "row", "columns", "both", and "neither" (default).
+#' @param rev reverse order of cells. Options are "row", "columns", "both", and "neither" (default). Option not available if supplying a table or matrix.
 #' @param dnn optional character vector of dimension names.
 #'
 #' @return a list with class "rdiff" containing the following components:
@@ -30,50 +29,90 @@
 #'
 #' @importFrom stats binom.test pnorm prop.test qnorm xtabs
 #' @export
-riskdiff <- function(df, x = NULL, y = NULL, weight = NULL, conf.level = 0.95,
-                     rev = c("neither", "rows", "columns", "both"), dnn = NULL) {
+riskdiff <- function(...) UseMethod("riskdiff")
 
+#' @inheritParams riskdiff
+#' @export
+#' @rdname riskdiff
+riskdiff.data.frame <- function(df, x = NULL, y = NULL, weight = NULL, conf.level = 0.95,
+                                rev = c("neither", "rows", "columns", "both"), dnn = NULL) {
   x <- rlang::enexpr(x)
   y <- rlang::enexpr(y)
   weight <- rlang::enexpr(weight)
 
-  # error checks
-  if (is.matrix(x) && !is.null(y)) {
-    stop("If x is a matrix, y should be NULL")
-  } else if (is.matrix(x) && is.null(y)) {
-    if (ncol(x) != 2L) {
-      stop("If using a matrix, dimensions must be 2x2")
-    } else if (nrow(x) != 2L) {
-      stop("If using a matrix, dimensions must be 2x2")
-    }
-  }
-
-  # frequency table
   tab <- tobyto(df = df, x = x, y = y, weight = weight, rev = rev, dnn = dnn)
 
-  # rd and conf interval
   p1 <- tab[1L, 1L] / sum(tab[1L, ])
   p2 <- tab[2L, 1L] / sum(tab[2L, ])
   rd <- round(p1 - p2, 4L)
   z <- qnorm(0.5 * (1L + conf.level))
   se <- sqrt((p1*(1L - p1)/sum(tab[1L, ])) + (p2*(1L - p2)/sum(tab[2L, ])))
 
-  if (rd < 0L)
-    ci <- rd + c(1L, -1L) * z * se
-  else if (rd > 0)
-    ci <- rd + c(-1L, 1L) * z * se
+  if (rd < 0L) ci <- rd + c(1L, -1L) * z * se
+  else if (rd > 0L) ci <- rd + c(-1L, 1L) * z * se
 
-  if (ci[1L] < -1L) {
-    ci[1L] <- -1L
-  } else if (ci[2L] > 1L) {
-    ci[2L] <- 1L
+  if (ci[1L] < -1L) ci[1L] <- -1L
+  else if (ci[2L] > 1L) ci[2L] <- 1L
+
+  to_print <- list(rd = rd, conf.level = conf.level, ci = ci,
+              p1 = p1, p2 = p2, tab = tab)
+  class(to_print) <- "rdiff"
+  to_print
+}
+
+#' @inheritParams riskdiff
+#' @export
+#' @rdname riskdiff
+riskdiff.table <- function(df, conf.level = 0.95, dnn = NULL) {
+  tab <- df
+  p1 <- tab[1L, 1L] / sum(tab[1L, ])
+  p2 <- tab[2L, 1L] / sum(tab[2L, ])
+  rd <- round(p1 - p2, 4L)
+  z <- qnorm(0.5 * (1L + conf.level))
+  se <- sqrt((p1*(1L - p1)/sum(tab[1L, ])) + (p2*(1L - p2)/sum(tab[2L, ])))
+
+  if (rd < 0L) ci <- rd + c(1L, -1L) * z * se
+  else if (rd > 0L) ci <- rd + c(-1L, 1L) * z * se
+
+  if (ci[1L] < -1L) ci[1L] <- -1L
+  else if (ci[2L] > 1L) ci[2L] <- 1L
+
+  to_print <- list(rd = rd, conf.level = conf.level, ci = ci,
+                   p1 = p1, p2 = p2, tab = tab)
+  class(to_print) <- "rdiff"
+  to_print
+}
+
+#' @inheritParams riskdiff
+#' @export
+#' @rdname riskdiff
+riskdiff.matrix <- function(df, conf.level = 0.95, dnn = NULL) {
+  tab <- df
+  p1 <- tab[1L, 1L] / sum(tab[1L, ])
+  p2 <- tab[2L, 1L] / sum(tab[2L, ])
+  rd <- round(p1 - p2, 4L)
+  z <- qnorm(0.5 * (1L + conf.level))
+  se <- sqrt((p1*(1L - p1)/sum(tab[1L, ])) + (p2*(1L - p2)/sum(tab[2L, ])))
+
+  rname <- c("Exposed", "Unexposed")
+  cname <- c("Outcome", "No Outcome")
+
+  if (is.null(dnn)) dimnames(tab) <- list(Exposure = rname, Outcome = cname)
+  else {
+    dimnames(tab) <- list(rname, cname)
+    names(dimnames(tab)) <- dnn
   }
 
-  out <- list(rd = rd, conf.level = conf.level, ci = ci,
-              p1 = p1, p2 = p2, tab = tab)
-  class(out) <- "rdiff"
-  out
+  if (rd < 0L) ci <- rd + c(1L, -1L) * z * se
+  else if (rd > 0L) ci <- rd + c(-1L, 1L) * z * se
 
+  if (ci[1L] < -1L) ci[1L] <- -1L
+  else if (ci[2L] > 1L) ci[2L] <- 1L
+
+  to_print <- list(rd = rd, conf.level = conf.level, ci = ci,
+                   p1 = p1, p2 = p2, tab = tab)
+  class(to_print) <- "rdiff"
+  to_print
 }
 
 #' @inheritParams riskdiff
