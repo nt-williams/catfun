58648cc4843cca712b9a937fa0f90373453c9524
Nicholas Williams <ntwilliams.personal@gmail.com>
2019-05-08 11:01:03 -0400

    refactored prop_test to use S3

--- R/prop_test.R
+++ R/prop_test.R
@@ -1,10 +1,10 @@
 #' Tests for equality of proportions
 #'
-#' Run tests for equality of proportions.
+#' Conduct 1-sample tests of proportions and tests for equality of k proportions.
 #'
 #' @param x a vector of counts, a one-dimensional table with two entries, or a two-dimensional table with 2 columns.
 #' @param n a vector of counts of trials, ignored if x is a matrix or table.
-#' @param p a vector of probabilities, length must be same as the number of groups specified by x.
+#' @param p a probability for the null hypothesis when testing a single proportion; ignored if comparing multiple proportions.
 #' @param method a character string indicating method for calculating confidence interval, default is "wald".
 #' @param alternative character string specifiying the alternative hypothesis. Possible options are "two.sided" (default),
 #'      "greater", or "less".
@@ -15,6 +15,7 @@
 #' @examples
 #' prop_test(7, 50, method = "wald", p = 0.2)
 #' prop_test(7, 50, method = "wald", p = 0.2, exact = TRUE)
+#' prop_test(c(23, 24), c(50, 55))
 #'
 #' vietnam <- data.frame(
 #'    service = c(rep("yes", 2), rep("no", 2)),
@@ -40,43 +41,36 @@
 #'
 #' @importFrom stats binom.test pnorm prop.test qnorm xtabs
 #' @export
-prop_test <- function(x, n, p = NULL,
-                      method = c("wald", "wilson", "agresti-couli", "jeffreys",
-                                 "modified wilson", "wilsoncc", "modified jeffreys",
-                                 "clopper-pearson", "arcsine", "logit", "witting", "pratt"),
-                      alternative = c("two.sided", "less", "greater"),
-                      conf.level = 0.95, correct = FALSE, exact = FALSE) {
+prop_test <- function(...) UseMethod("prop_test")
+
+#' @inheritParams prop_test
+#' @export
+#' @rdname prop_test
+prop_test.numeric <- function(x, n, p = .5,
+                              method = c("wald", "wilson", "agresti-couli", "jeffreys",
+                                         "modified wilson", "wilsoncc", "modified jeffreys",
+                                         "clopper-pearson", "arcsine", "logit", "witting", "pratt"),
+                              alternative = c("two.sided", "less", "greater"),
+                              conf.level = 0.95, correct = FALSE, exact = FALSE) {
 
   method <- match.arg(method)
   alternative <- match.arg(alternative)
 
-  if (is.matrix(x)) {
-    n <- rowSums(x)
-    x <- x[, 1L]
-  }
-
   if (length(x) == 1L) {
-    if (!is.null(p)) {
-      p <- p
-    } else {
-      p <- 0.5
-    }
 
     exact_test <- binom.test(x, n, p = p, alternative = alternative, conf.level = conf.level)
-    if (exact == TRUE) {
-      exact_p <- round(exact_test$p.value, 5L)
-    } else {
-      exact_p <- NULL
-    }
+    if (exact == TRUE) exact_p <- round(exact_test$p.value, 5L)
+    else exact_p <- NULL
+
     exact_ci <- exact_test$conf.int[1L:2L]
-  } else if (length(x) > 1L && is.null(p)) {
+  } else if (length(x) > 1L) {
+    p <- NULL
     exact_ci <- NULL
     exact_p <- NULL
   }
 
-  if (correct == FALSE) {
-    test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = FALSE)
-  } else {
+  if (correct == FALSE) test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = FALSE)
+  else {
     test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = TRUE)
     method <- "wilsoncc"
   }
@@ -95,10 +89,98 @@ prop_test <- function(x, n, p = NULL,
     names(method_ci) <- c("Lower bound", "Upper bound")
   }
 
-  out <- list(x = x, n = n, p = p, estimate = estimate, method = method, method_ci = method_ci,
+  to_print <- list(x = x, n = n, p = p, estimate = estimate, method = method, method_ci = method_ci,
               exact_ci = exact_ci, exact_p = exact_p, statistic = statistic, df = df, p_value = p_value)
-  class(out) <- "prop_test"
-  out
+  class(to_print) <- "prop_test"
+  to_print
+
+}
+
+#' @inheritParams prop_test
+#' @export
+#' @rdname prop_test
+prop_test.table <- function(x, n, p = NULL,
+                            method = c("wald", "wilson", "agresti-couli", "jeffreys",
+                                       "modified wilson", "wilsoncc", "modified jeffreys",
+                                       "clopper-pearson", "arcsine", "logit", "witting", "pratt"),
+                            alternative = c("two.sided", "less", "greater"),
+                            conf.level = 0.95, correct = FALSE, exact = FALSE) {
+
+  method <- match.arg(method)
+  alternative <- match.arg(alternative)
+
+  n <- rowSums(x)
+  x <- x[, 1L]
+
+  exact_ci <- NULL
+  exact_p <- NULL
+
+  if (correct == FALSE) {
+    test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = FALSE)
+  } else {
+    test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = TRUE)
+    method <- "wilsoncc"
+  }
+
+  statistic <- test$statistic
+  df <- test$parameter
+  p_value <- round(test$p.value, 5L)
+  estimate <- round(test$estimate, 4L)
+
+  ci <- DescTools::BinomCI(x, n, method = method, conf.level = conf.level)
+  method_ci <- ci[, 2L:3L]
+
+  rownames(method_ci) <- c()
+  method_ci <- as.data.frame(method_ci)
+  names(method_ci) <- c("Lower bound", "Upper bound")
+
+  to_print <- list(x = x, n = n, p = p, estimate = estimate, method = method, method_ci = method_ci,
+              exact_ci = exact_ci, exact_p = exact_p, statistic = statistic, df = df, p_value = p_value)
+  class(to_print) <- "prop_test"
+  to_print
+}
+
+#' @inheritParams prop_test
+#' @export
+#' @rdname prop_test
+prop_test.matrix <- function(x, n, p = NULL,
+                             method = c("wald", "wilson", "agresti-couli", "jeffreys",
+                                        "modified wilson", "wilsoncc", "modified jeffreys",
+                                        "clopper-pearson", "arcsine", "logit", "witting", "pratt"),
+                             alternative = c("two.sided", "less", "greater"),
+                             conf.level = 0.95, correct = FALSE, exact = FALSE) {
+
+  method <- match.arg(method)
+  alternative <- match.arg(alternative)
+
+  n <- rowSums(x)
+  x <- x[, 1L]
+
+  exact_ci <- NULL
+  exact_p <- NULL
+
+  if (correct == FALSE) test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = FALSE)
+  else {
+    test <- prop.test(x = x, n = n, p = p, alternative = alternative, correct = TRUE)
+    method <- "wilsoncc"
+  }
+
+  statistic <- test$statistic
+  df <- test$parameter
+  p_value <- round(test$p.value, 5L)
+  estimate <- round(test$estimate, 4L)
+
+  ci <- DescTools::BinomCI(x, n, method = method, conf.level = conf.level)
+  method_ci <- ci[, 2L:3L]
+
+  rownames(method_ci) <- c()
+  method_ci <- as.data.frame(method_ci)
+  names(method_ci) <- c("Lower bound", "Upper bound")
+
+  to_print <- list(x = x, n = n, p = p, estimate = estimate, method = method, method_ci = method_ci,
+              exact_ci = exact_ci, exact_p = exact_p, statistic = statistic, df = df, p_value = p_value)
+  class(to_print) <- "prop_test"
+  to_print
 }
 
 #' @inheritParams prop_test
@@ -124,7 +206,6 @@ print.prop_test <- function(x, ...) {
     if (!is.null(x$exact_p)) {
       cat("Exact p-value:", x$exact_p, "\n")
     }
-    cat("\n")
   } else {
     cat("Observed proportions:", "\n")
     cli::cat_bullet(x$estimate)
@@ -136,6 +217,7 @@ print.prop_test <- function(x, ...) {
     cat(length(x$x), "sample test for equality of proportions", "\n")
     cat("Chi-squared:", x$statistic, "\n")
     cat("p-value:", x$p_value)
+    cli::cat_line()
   }
 
 }
